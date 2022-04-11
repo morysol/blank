50190d8995cc491b5edd45a7fc14e9fea9d15cf8
morysol <cepera.cMupHoB@gmail.com>
2022-04-11 20:32:19 +0300

    13

--- js/13_test_context.js
+++ js/13_test_context.js
@@ -1,5 +1,40 @@
-function foo() {
-  console.log(this);
+// function foo() {
+//   console.log(this);
+// }
+
+// foo();
+
+// const petya = {
+//   username: 'Petya',
+//   showThis() {
+//     console.log(this);
+//   },
+//   showName() {
+//     console.log(this.username);
+//   },
+// };
+
+// petya.showThis(); // {username: "Petya", showThis: ƒ, showName: ƒ}
+// petya.showName(); // 'Petya'
+
+function showThis() {
+  console.log('this in showThis: ', this);
 }
 
-foo();
+const showThisArrow = () => {
+  console.log('this in showThis: ', this);
+};
+
+showThis();
+showThisArrow();
+
+const user = {
+  username: 'Mango',
+};
+
+user.showContext = showThis;
+user.showContextArrow = showThisArrow;
+// console.log(user);
+// console.log(user.showContext);
+user.showContext(); // this in showThis: {username: "Mango", showContext: ƒ}
+user.showContextArrow(); // undefined
